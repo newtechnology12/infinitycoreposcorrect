c82aabcfd745dab49546f0c90b08a99342ac43e2
newtechnology12 <amazingtechnology00@gmail.com>
2025-01-18 16:57:59 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,3 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react-swc";
-// import path from "path";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: true,
-//   },
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-//   build: {
-//     outDir: "./pocketbase/pb_public",
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";

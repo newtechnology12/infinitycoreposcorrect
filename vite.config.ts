8aac232a6a3ed3f375f14581bcc04de42a17c292
newtechnology12 <amazingtechnology00@gmail.com>
2025-01-18 16:52:00 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+//   build: {
+//     outDir: "./pocketbase/pb_public",
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,6 +32,8 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "./pocketbase/pb_public",
+    outDir: "dist",  
+    sourcemap: true, 
+    chunkSizeWarningLimit: 1600, 
   },
 });

6894b9cf7822a833c1369cb7df1e93e95f859704
newtechnology12 <amazingtechnology00@gmail.com>
2025-01-18 20:41:53 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -13,8 +14,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "dist", // Ensure this matches your expected output directory
-    sourcemap: true, // Enable sourcemaps for debugging
-    chunkSizeWarningLimit: 1600, // Increase the limit if needed
+    outDir: "./pocketbase/pb_public",
   },
 });
